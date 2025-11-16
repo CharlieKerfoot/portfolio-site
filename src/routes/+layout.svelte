7b76b279c15ce228ac108b7ed54fe6ea69308649
charliekerfoot <charliekerfoot@gmail.com>
2025-11-16 11:49:54 -0600

    Under Construction

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link
+		rel="icon"
+		type="image/png"
+		href="/src/lib/assets/favicon/favicon-96x96.png"
+		sizes="96x96"
+	/>
+	<link rel="icon" type="image/svg+xml" href="/src/lib/assets/favicon/favicon.svg" />
+	<link rel="shortcut icon" href="/src/lib/assets/favicon/favicon.ico" />
+	<link
+		rel="apple-touch-icon"
+		sizes="180x180"
+		href="/src/lib/assets/favicon/apple-touch-icon.png"
+	/>
+	<link rel="manifest" href="/src/lib/assets/favicon/site.webmanifest" />
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<div class="main">
+	<h1>Under Construction</h1>
+
+	<p>
+		Hey, my name is Charlie Kerfoot. I have a deep passion for software development and deep
+		learning.
+	</p>
+	<p>
+		Currently, my new portfolio site is under construction. In the meantime, here is my <a
+			target="_blank"
+			href="https://drive.google.com/file/d/1GdAZf3h2w-vWGj55r0wWwArTD-C2SzVp/view?usp=sharing"
+			>CV</a
+		>
+		and please feel free to
+		<a target="_blank" href="mailto:charliekerfoot@gmail.com">reach out</a>
+		or contact me on
+		<a target="_blank" href="https://linkedin.com/in/ckerf">LinkedIn</a> with any inquiries.
+	</p>
+</div>
+
+<style>
+	.main {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background-color: rgb(231, 227, 221);
+	}
+
+	h1 {
+		font-size: clamp(2.5rem, 10vw, 8rem);
+		font-weight: bold;
+		margin-bottom: 4rem;
+	}
+
+	@media (max-width: 600px) {
+		p {
+			font-size: 1rem;
+			padding: 0 1rem;
+			margin-bottom: 1rem;
+		}
+	}
+
+	p {
+		text-align: center;
+		font-size: 1.12em;
+		font-family: 'Tinos', serif;
+	}
+
+	a {
+		text-decoration: underline rgb(100, 100, 100);
+	}
+</style>
