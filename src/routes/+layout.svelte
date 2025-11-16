5509caab6ec5378b27bfe4a10965962abdc4e319
charliekerfoot <charliekerfoot@gmail.com>
2025-11-16 12:08:51 -0600

    Fixed favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,20 +5,11 @@
 </script>
 
 <svelte:head>
-	<link
-		rel="icon"
-		type="image/png"
-		href="/src/lib/assets/favicon/favicon-96x96.png"
-		sizes="96x96"
-	/>
-	<link rel="icon" type="image/svg+xml" href="/src/lib/assets/favicon/favicon.svg" />
-	<link rel="shortcut icon" href="/src/lib/assets/favicon/favicon.ico" />
-	<link
-		rel="apple-touch-icon"
-		sizes="180x180"
-		href="/src/lib/assets/favicon/apple-touch-icon.png"
-	/>
-	<link rel="manifest" href="/src/lib/assets/favicon/site.webmanifest" />
+	<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+	<link rel="manifest" href="/favicon/site.webmanifest" />
 </svelte:head>
 
 {@render children()}
